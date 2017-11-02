5beb4db91cdf39ba850c18dc10c583b2195d6b82
Luis Lara <luislpatino@gmail.com>
2017-11-01 20:16:37 -0600

    Documenta ejecutarSigAccion

--- robot.pl
+++ robot.pl
@@ -10,4 +10,8 @@
 % Alejandro Ehecatl Morales Huitrón
 % =============================================================================
 
+% Simula la ejecución de la siguiente acción en la agenda del robot, y la
+% retira de la misma. Si la ejecución de la acción fracasa, la acción no se
+% elimina de la agenda, pero el predicado es verdadero. El predicado falla
+% cuando ya no hay ninguna acción por realizar.
 ejecutarSigAccion(Base, NuevaBase).
