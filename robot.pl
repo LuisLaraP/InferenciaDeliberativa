632a3b1dc033b0de8d5d2c489fcc2bfaf037a8a8
Luis Lara <luislpatino@gmail.com>
2017-11-04 19:46:18 -0600

    Mueve accionPendiente y ejecutarPlan a simulador

--- robot.pl
+++ robot.pl
@@ -9,11 +9,3 @@
 % Roberto Monroy Argumedo
 % Alejandro Ehecatl Morales Huitrón
 % =============================================================================
-
-% Este predicado es verdadero si el robot aún tiene acciones pendientes por
-% realizar.
-accionPendiente(_).
-
-% Ejecuta las acciones contenidas en la agenda del robot, hasta que éstas se
-% agoten o bien, hasta que ocurra un error.
-ejecutarPlan(_, _).
--- simulador.pl
+++ simulador.pl
@@ -19,3 +19,11 @@ simulador(Base, NuevaBase) :-
 	planeacion(BaseB, BaseC),
 	ejecutarPlan(BaseC, BasePlan),
 	simulador(BasePlan, NuevaBase).
+
+% Este predicado es verdadero si el robot aún tiene acciones pendientes por
+% realizar.
+accionPendiente(_).
+
+% Ejecuta las acciones contenidas en la agenda del robot, hasta que éstas se
+% agoten o bien, hasta que ocurra un error.
+ejecutarPlan(_, _).
