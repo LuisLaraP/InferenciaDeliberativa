ea1272518ae031ab3ba9d9b3c20e48f76a437244
Luis Lara <luislpatino@gmail.com>
2017-11-23 08:13:02 -0600

    Implementa el filtrado inteligente de objetivos

--- decision.pl
+++ decision.pl
@@ -34,13 +34,20 @@ decision(Base, NuevaBase) :-
 	fusionarObjetivos(Entregas, Reacomodos, DecBase),
 	filtrar(decisionCumplida(Base), DecBase, Cumplidas),
 	eliminarTodos(Cumplidas, DecBase, Decisiones),
+	filtrarObjetivos(Decisiones, Base, Filtradas),
 	writeln('Se tomaron las siguientes decisiones:'),
-	imprimirLista(Decisiones), nl,
+	imprimirLista(Filtradas), nl,
 	buscar(objeto([decision], _, _, _), Base, objeto(_, A, P, R)),
 	reemplazar(
 		objeto([decision], A, P, R),
-		objeto([decision], A, Decisiones, R),
-		Base, NuevaBase
+		objeto([decision], A, Filtradas, R),
+		Base, Base2
+	),
+	buscar(objeto([diagnostico], _, _, _), Base2, objeto(_, Ad, Pd, Rd)),
+	reemplazar(
+		objeto([diagnostico], Ad, Pd, Rd),
+		objeto([diagnostico], Ad, [], Rd),
+		Base2, NuevaBase
 	).
 
 expandirEntregas([], []).
@@ -55,6 +62,27 @@ fusionarObjetivos([], Reacomodos, Reacomodos).
 fusionarObjetivos([entregar(O) | Es], Reacomodos, [entregar(O) | Rs]) :-
 	fusionarObjetivos(Es, Reacomodos, Rsig),
 	eliminar(reacomodar(O), Rsig, Rs).
+
+filtrarObjetivos([], _, []).
+filtrarObjetivos([O | _], Base, [O]) :-
+	O =.. [_ , Arg],
+	objetosAgarrados(Base, Ag),
+	estaEn(Ag, Arg),
+	writeln('Se realizará el primer objetivo por ahora.'), !.
+filtrarObjetivos(Objetivos, _, Objetivos) :-
+	length(Objetivos, Longitud),
+	Longitud < 2, !.
+filtrarObjetivos([O1, O2 | _], Base, Nuevos) :-
+	O1 =.. [_, Arg1],
+	O2 =.. [_, Arg2],
+	ubicacionObjeto(Arg1, Base, U1),
+	ubicacionObjeto(Arg2, Base, U2),
+	(U1 == U2
+	->	Nuevos = [O1, O2],
+		writeln('Se realizarán los dos primeros objetivos por ahora.')
+	;	Nuevos = [O1],
+		writeln('Se realizará el primer objetivo por ahora.')
+	).
 
 objetosAgarrados(Base, Objetos) :-
 	propiedadesObjeto(robot, Base, Props),
